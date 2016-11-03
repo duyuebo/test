3cb8be248c99e3576862f95efe339c0643f449ac
杜月波 <469563628@qq.com>
2016-11-04 07:55:16 +0800

    pdf中文测试

--- pdtTest/src/main/java/com/dyb/pdf/PdfTest.java
+++ pdtTest/src/main/java/com/dyb/pdf/PdfTest.java
@@ -5,12 +5,15 @@
 package com.dyb.pdf;
 
 
-import org.apache.poi.wp.usermodel.Paragraph;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import javax.swing.text.Document;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Set;
 import java.util.UUID;
 /**
  *
@@ -28,22 +31,107 @@ public class PdfTest {
     }
 
     public static void printText(String dest){
-        PdfWriter writer = null;
         try {
-            writer = new PdfWriter(dest);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
-            //Paragraph cn = new Paragraph("English");
-            Paragraph cn = new Paragraph("这是中文");
-            PdfFontFactory.register("D:\\MyConfiguration\\dyb37716\\Downloads\\songti.ttf","songti");
-            Set<String> fonts = PdfFontFactory.getRegisteredFonts();
-            for(String s:fonts){
-                System.out.println(s);
-            }
-            PdfFont font = PdfFontFactory.createRegisteredFont("经典宋体简");
-            cn.setFont(font);
-            document.add(cn);
+            Document document = new Document(PageSize.A4,80, 79, 60, 45);
+            PdfWriter.getInstance(document, new FileOutputStream(dest));
+            document.open();
+            BaseFont bfChinese = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", false);
+            Font fontChinese = new Font(bfChinese);
+            Font titleChinese = new Font(bfChinese, 20, Font.BOLD);
+            Font normalChinese = new Font(bfChinese, 14, Font.NORMAL);
+            Font subBoldFontChinese = new Font(bfChinese, 8, Font.BOLD);
+            Font subFontChinese = new Font(bfChinese, 12, Font.NORMAL);
+
+            Paragraph title = new Paragraph("订单",titleChinese);
+            title.setAlignment(Element.ALIGN_CENTER);
+            title.setLeading(1f);
+            document.add(title);
+
+            Paragraph content = new Paragraph("订单号：1234512363", normalChinese);
+            content.setSpacingBefore(25f);//设置上面空白宽度
+            document.add(content);
+
+            content = new Paragraph("客户姓名：杜月波    客户电话：18896740989", normalChinese);
+            content.setSpacingBefore(5f);//设置上面空白宽度
+            document.add(content);
+
+            content = new Paragraph("商品详情：", normalChinese);
+            content.setSpacingBefore(5f);//设置上面空白宽度
+            document.add(content);
+
+            //设定6列
+            float[] widths = new float[]{ 14f,30f,8f,15f,15f,8f };
+            PdfPTable table = new PdfPTable(widths);
+            table.setSpacingBefore(5f);// 设置表格上面空白宽度
+            //table.setTotalWidth(500);// 设置表格的宽度
+            table.setWidthPercentage(100);//设置表格宽度为%100
+
+            //PdfPTable tableTest = new PdfPTable(8);
+            PdfPCell cell = new PdfPCell(new Paragraph("编码", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            cell = new PdfPCell(new Paragraph("商品名称", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            cell = new PdfPCell(new Paragraph("单位", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            cell = new PdfPCell(new Paragraph("型号", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            cell = new PdfPCell(new Paragraph("规格", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            cell = new PdfPCell(new Paragraph("数量", subBoldFontChinese));//描述
+            cell.setFixedHeight(20);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+            table.addCell(cell);
+            document.add(table);
+
+//            int rowCount=1; //行计数器
+//            PdfPCell cell = null;
+//            //---表头
+//            cell = new PdfPCell(new Paragraph("期次", subBoldFontChinese));//描述
+//            cell.setFixedHeight(20);
+//            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+//            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+//            table.addCell(cell);
+//            cell = new PdfPCell(new Paragraph("租金日", subBoldFontChinese));//描述
+//            cell.setFixedHeight(20);
+//            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+//            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+//            table.addCell(cell);
+//            cell = new PdfPCell(new Paragraph("各期租金金额", subBoldFontChinese));//描述
+//            cell.setFixedHeight(20);
+//            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+//            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+//            table.addCell(cell);
+//
+//            cell = new PdfPCell(new Paragraph("各期租金后\n剩余租金", subBoldFontChinese));//描述
+//            cell.setHorizontalAlignment(Element.ALIGN_CENTER);// 设置内容水平居中显示
+//            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);  // 设置垂直居中
+//            cell.setFixedHeight(20);
+//            table.addCell(cell);
+//            document.add(table);
+//            Chunk chunk = new Chunk("中文", fontChinese);
+//            Chapter chapter = new Chapter(new Paragraph(chunk), 1);
+//            chapter.setNumberDepth(0);
+//            chapter.add(new Paragraph("This is the paragraph", fontChinese));
+//            document.add(chapter);
             document.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
